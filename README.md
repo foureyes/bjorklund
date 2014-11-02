37500f3612dd6e27a370a63cf2e3e7fc8e301c32
jversoza <jjv222@nyu.edu>
2014-11-02 12:00:32 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 bjorklund
 =========
+
+### Overview
+
+A super quick implementation of [Bjorklund's algorithm](https://ics-web.sns.ornl.gov/timing/Rep-Rate%20Tech%20Note.pdf).
+
+### Usage
+
+<code>
+var pulses = 5,
+	steps = 8;
+
+console.log(bjorklund.generate(pulses, steps));
+// returns [ 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0 ]
+</code>
---
+++ bjorklund.js
@@ -0,0 +1,49 @@
+var bjorklund = (function() {
+
+  var generate = function(numPulses, numSteps) {
+    var PULSE = '1',
+      STEP = '0';
+      pulses = repeat([PULSE], numPulses);
+      steps = repeat([STEP], numSteps);
+      pattern = distribute(steps, pulses);
+
+    return pattern.reduce(function(accum, ele){
+      return accum.concat(ele.split('').map(function(s) {
+        return +s;
+      }));
+    }, []);
+  };
+  
+  var distribute = function(dividend, remainder) {
+    var newDividend = [],
+      newRemainder = [];
+    if (remainder.length > 1) {  
+      newRemainder = repeat([dividend[0]], dividend.length % remainder.length);
+      remainder.forEach(function (ele) {
+        newDividend.push("" + dividend[0] + ele);
+      });
+      return distribute(newDividend, newRemainder);
+    } else {
+      return remainder ? dividend.concat(remainder) : dividend;
+    }
+  };
+  
+  var repeat = function (arr, repetitions) {
+    var repeated;
+    if (repetitions > 0) {
+      repeated = arr.slice();
+      for (var i = 0; i < repetitions - 1; i++) {
+        repeated = repeated.concat(arr);
+      }
+    } else {
+      repeated = [];
+    }
+    return repeated;
+  };
+
+  return {
+    generate: generate
+  };
+})();
+
+console.log(bjorklund.generate(pulses, steps));
